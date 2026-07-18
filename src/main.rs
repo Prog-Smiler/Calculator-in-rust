5a4bb53d528fac8a6c46b2dfff5128da6f872d67
Prog-Smiler <progsmiler@gmail.com>
2026-07-18 22:09:29 +0545

    Calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    print!("Enter the first number: ");
+    io::Write::flush(&mut io::stdout()).expect("flush failed");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let number1: f32 = input.trim().parse().expect("Enter a valid number!");
+    input.clear();
+
+    print!("Enter the second number: ");
+    io::Write::flush(&mut io::stdout()).expect("flush failed");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let number2: f32 = input.trim().parse().expect("Enter a valid number!");
+
+    println!("Addition: {}", number1 + number2);
+    println!("Substraction: {}", number1 - number2);
+    println!("Multiplication: {}", number1 * number2);
+    println!("Divison (quotient): {}", number1 / number2);
+    println!("Divison (remainder): {}", number1 % number2)
 }
