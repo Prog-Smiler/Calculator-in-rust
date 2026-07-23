9d8c94ca466cf0308a7aaa790925950bdcd31a91
Prog-Smiler <progsmiler@gmail.com>
2026-07-23 18:27:01 +0545

    Final

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-This is a simple project in rust just created to understand how rust works.
-The program askes you to enter two different numder and after you input those numbers the program does some calcualtions like addition, substraction, multiplication, divison and it prints it out on the screen.
+# Why this project was made?
+-> This is a simple project in rust just created to understand how rust works. 
+
+# How does this work?
+-> The program askes you to enter two different numder and after you input those numbers the program does some calcualtions like addition, substraction, multiplication, divison and it prints it out on the screen.
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 use std::io;
+use std::io::Write;
 
 fn main() {
-    let mut input = String::new();
-
-    print!("Enter the first number: ");
-    io::Write::flush(&mut io::stdout()).expect("flush failed");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let number1: f32 = input.trim().parse().expect("Enter a valid number!");
-    input.clear();
-
-    print!("Enter the second number: ");
-    io::Write::flush(&mut io::stdout()).expect("flush failed");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let number2: f32 = input.trim().parse().expect("Enter a valid number!");
+    let number1 = get_f32_input("Enter the first number: ");
+    let number2 = get_f32_input("Enter the second number: ");
 
     println!("Addition: {}", number1 + number2);
     println!("Substraction: {}", number1 - number2);
     println!("Multiplication: {}", number1 * number2);
-    println!("Divison (quotient): {}", number1 / number2);
-    println!("Divison (remainder): {}", number1 % number2);
+    if number2 != 0.0 {
+        println!("Divison (quotient): {}", number1 / number2);
+        println!("Divison (remainder): {}", number1 % number2);
+    } else {
+        println!("Not divisible by zero");
+    }
+}
+
+//function for input and managing if user entered a very large number
+fn get_f32_input(prompt: &str) -> f32 {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().expect("Flush failed");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        if let Ok(num) = input.trim().parse::<f32>() {
+            if num.is_infinite() {
+                println!("Error: Num is too large! Max is ~3.4e38.");
+            } else {
+                return num;
+            }
+        } else {
+            println!("Enter a valid number! ");
+        }
+    }
 }
